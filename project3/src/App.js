7c8f048c0ae86e8bf4919c5d1b5699f15c4f4a23
Michael <michaelgandolfi7@gmail.com>
2023-01-28 10:29:40 -0600

    complete project3

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import Joke from './components/Joke'
+import jokesData from './jokesData'
 
 export default function App() {
+  const jokeElements = jokesData.map(joke => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />
+  })
   return (
     <div>
-      <Joke
-        setup="I got my daughter a fridge for her birthday." 
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Joke
-        setup="How did the hacker escape the police?" 
-        punchline="He ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?" 
-        punchline="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?" 
-        punchline="Swarm."
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?" 
-        punchline="I don't know, but the flag is a big plus"
-      />
+      {jokeElements}
     </div>
-    
   )
-};
+}
 
