e09a8829e5c14d20a64101a205db386611c7286d
Michael <michaelgandolfi7@gmail.com>
2023-01-30 08:56:09 -0600

    add random image to project4

--- index.js
+++
@@ -1,1 +0,0 @@
-ReactDOM.render(<ul><li>1</li><li>2</li></ul>, document.getElementById('root'));
--- project4/src/components/Main.js
+++ project4/src/components/Main.js
@@ -23,7 +23,6 @@ function getMemeImage() {
   
 }
 
-
   return (
     <main>
       <div className="form">
